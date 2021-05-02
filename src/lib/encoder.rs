8387e636b3f546e948835d18fe684d5f645290d4
ph0llux <ph0llux@pm.me>
2021-05-02 20:37:39 +0200

    base64 support for &str added

--- src/lib/encoder.rs
+++ src/lib/encoder.rs
@@ -1,9 +1,12 @@
+/*************************************************************************
+* ph0llux:1c7c5d99aa43ea2534b724243f41e58037126d37ec94794d67625b4b22219263
+*************************************************************************/
 /************************************************************************
 * pk:0be5980330abddd340e3d6384103add1aa9ab63f32c48a8b4cc5d71cbcf0595b
 ***********************************************************************/
 
 // - external
-use base64::encode;
+use base64::{encode};
 
 pub trait ToBase64 {
 	/// method converts the input to a base64 encoded [String].
@@ -20,6 +23,12 @@ pub trait ToBase64 {
 }
 
 impl ToBase64 for String {
+	fn base64(self) -> String {
+		encode(self.as_bytes())
+	}
+}
+
+impl ToBase64 for &str {
 	fn base64(self) -> String {
 		encode(self.as_bytes())
 	}
