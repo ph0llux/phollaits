a55db6108f8702577c3defc281f0a455353fdb16
ph0llux <ph0llux@pm.me>
2021-06-06 11:00:24 +0200

    hexifier added; fixed doc/test error in ToBase64

--- src/lib/encoder.rs
+++ src/lib/encoder.rs
@@ -1,9 +1,10 @@
 /*************************************************************************
-* ph0llux:c08c5261c7afd62c018cd50059b5ecd3fcb16cec279b4a666579932bf9864fc7
+* ph0llux:24efd740f491bae39a06ceb74c1f642f8d93b8e4aebdf03f95e32031b4f7ceea
 *************************************************************************/
 
 // - external
 use base64::{encode};
+use hex::ToHex as HexToHex;
 
 pub trait ToBase64 {
 	/// method converts the input to a base64 encoded [String].
@@ -14,7 +15,7 @@ pub trait ToBase64 {
 	/// use phollaits::{ToBase64};
 	///	fn main() {
 	///		let a = "Teststring".to_string();
-	///		asserteq!(a.base64(), "VGVzdHN0cmluZw==".to_string());
+	///		assert_eq!(a.base64(), "VGVzdHN0cmluZw==".to_string());
 	///	}
 	fn base64(self) -> String;
 }
@@ -34,5 +35,40 @@ impl ToBase64 for &str {
 impl ToBase64 for Vec<u8> {
 	fn base64(self) -> String {
 		encode(self)
+	}
+}
+
+pub trait ToHex {
+	fn hexify(self) -> String;
+	fn hexify_upper(self) -> String;
+}
+
+impl<T: AsRef<[u8]>> ToHex for T {
+	/// converts a value (implementing AsRef<[u8]>; this includes String, str, Vec<u8> and [u8]) to hex.
+	/// # Example
+	/// ```
+	/// use phollaits::{ToHex};
+	///
+	/// fn main() {
+	///		let value = "myValue!";
+	///		assert_eq!(value.hexify(), "6d7956616c756521".to_string())
+	/// }
+	/// ```
+	fn hexify(self) -> String {
+		self.encode_hex::<String>()
+	}
+
+	/// converts a value (implementing AsRef<[u8]>; this includes String, str, Vec<u8> and [u8]) to hex (UPPER_CASE).
+	/// # Example
+	/// ```
+	/// use phollaits::{ToHex};
+	///
+	/// fn main() {
+	///		let value = "myValue!";
+	///		assert_eq!(value.hexify_upper(), "6D7956616C756521".to_string())
+	/// }
+	/// ```
+	fn hexify_upper(self) -> String {
+		self.encode_hex_upper::<String>()
 	}
 }
